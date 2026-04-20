b94af4acfd43c262b8ecfd626200c0cfba5a9c70
Vitaly Kolodchenko <kolodchenkovitaly250@gmail.com>
2026-04-20 10:53:57 +0300

    feat: update support screen keyboard layout and add navigation buttons

--- src/screens/support.ts
+++ src/screens/support.ts
@@ -5,7 +5,17 @@ import { packCb } from '../core/callback.js'
 import type { ScreenView } from '../core/render.js'
 
 export function supportScreen(userId: number): ScreenView {
-  const kb = new InlineKeyboard().text('❌ Завершить диалог', packCb({ a: 'end_support' })).row()
+  const kb = new InlineKeyboard()
+
+    .url('Вопрос по подписке?', 'https://t.me/k1r4r1k')
+    .url('Вопрос по обучению?', 'https://t.me/imacport')
+    .row()
+
+    .url('Вопрос на счет программы?', 'https://t.me/+ZAMZ3oP2Cs41MGYy')
+    .row()
+  kb.text('◀️ Назад', packCb({ a: 'home' }))
+
+  // .text('❌ Завершить диалог', packCb({ a: 'end_support' })).row()
 
   return {
     photo: './public/profile.png',
